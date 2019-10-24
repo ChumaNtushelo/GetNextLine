b85159fbd8e66515ed0f9eab9352b5a2a752b755
Chuma NTUSHELO <cntushel@c5r4s6.wethinkcode.co.za>
2019-10-24 13:53:30 +0200

    get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cntushel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/26 09:53:20 by cntushel          #+#    #+#             */
+/*   Updated: 2019/09/18 08:28:11 by cntushel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include "./libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
